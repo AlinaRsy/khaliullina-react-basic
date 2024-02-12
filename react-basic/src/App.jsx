6a5bef1c8a60bde668d0e78c1c86d4e6b01aa761
AlinaRsy <al.alinarsy_616@mail.ru>
2024-02-12 22:17:54 +0300

    react2

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Banner from './components/Banner'
+import Header from './components/Header'
 
 function App() {
   return (
-    <p>Халиуллина Алина</p>
+    <>
+        <Header/>
+        <Banner/>
+    </>
   )
 }
 
