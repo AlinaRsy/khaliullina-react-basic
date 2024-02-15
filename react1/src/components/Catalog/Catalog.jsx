e1b37adb07afca0a4aa3d34f57e25062bbe11b0c
AlinaRsy <al.alinarsy_616@mail.ru>
2024-02-15 13:19:46 +0300

    react5

--- react1/src/components/Catalog/Catalog.jsx
+++ react1/src/components/Catalog/Catalog.jsx
@@ -1,21 +1,21 @@
 import s from './Catalog.module.css';
 import CatalogCard from './CatalogCard/CatalogCard';
-import t1 from '../../assets/imgs/products/t1.jpg';
-import t2 from '../../assets/imgs/products/t2.jpg';
-import t3 from '../../assets/imgs/products/t3.jpg';
-import t4 from '../../assets/imgs/products/t4.jpg';
-import t5 from '../../assets/imgs/products/t5.jpg';
-import t6 from '../../assets/imgs/products/t6.jpg';
+import {products} from '../../data';
 
 export default function Catalog(){
     return(
             <div class={s.catalog__inner}>
-                <CatalogCard productImg={t1} productName="Пиджак DIOR MOLLET" productPrice="900"/>
+                {products.map((card)=>{
+                    return <CatalogCard productImg={card.productImg} productName={card.productName} productPrice={card.productPrice}/>
+                })
+                    
+                }
+{/*                 
                 <CatalogCard productImg={t2} productName="Сумка CHANEL PMD " productPrice="1000"/>
                 <CatalogCard productImg={t3} productName="Платье PRADA KIKI" productPrice="1070"/>
                 <CatalogCard productImg={t4} productName="Брюки BALENCIAGA" productPrice="877"/>
                 <CatalogCard productImg={t5} productName="Рубашка BALENCIAGA" productPrice="699"/>
-                <CatalogCard productImg={t6} productName="Пиджак BALENCIAGA" productPrice="1399"/>
+                <CatalogCard productImg={t6} productName="Пиджак BALENCIAGA" productPrice="1399"/> */}
             </div>
     )
 }
---
+++ react1/src/data.js
@@ -0,0 +1,38 @@
+import t1 from './assets/imgs/products/t1.jpg';
+import t2 from './assets/imgs/products/t2.jpg';
+import t3 from './assets/imgs/products/t3.jpg';
+import t4 from './assets/imgs/products/t4.jpg';
+import t5 from './assets/imgs/products/t5.jpg';
+import t6 from './assets/imgs/products/t6.jpg';
+export const products = [
+    {
+        'productImg': t1,
+        'productName': 'CLOTHES 101',
+        'productPrice': 900
+    },
+    {
+        'productImg': t2,
+        'productName': 'CLOTHES 202',
+        'productPrice': 899
+    },
+    {
+        'productImg': t3,
+        'productName': 'CLOTHES 303',
+        'productPrice': 1266
+    },
+    {
+        'productImg': t4,
+        'productName': 'CLOTHES 404',
+        'productPrice': 799
+    },
+    {
+        'productImg': t5,
+        'productName': 'CLOTHES 505',
+        'productPrice': 1119
+    },
+    {
+        'productImg': t6,
+        'productName': 'CLOTHES 606',
+        'productPrice': 1322
+    },
+]
